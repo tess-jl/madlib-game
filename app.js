44ab34440ee5a8cbedb76927645b5fb5356fcd01
Tess Lameyer <tjlameyer@gmail.com>
2019-09-19 19:26:36 -0700

    set up all files and started project

---
+++ app.js
@@ -0,0 +1,16 @@
+function revealMadlib() {
+    const nounInput = document.getElementById('noun-input');
+    const noun = nounInput.value;
+    const nounSpan = document.getElementById('noun');
+    nounSpan.textContent = noun;
+
+    const verbInput = document.getElementById('verb-input');
+    const verb = verbInput.value;
+    const verbSpan = document.getElementById('verb');
+    verbSpan.textContent = verb;
+
+    const adjInput = document.getElementById('adj-input');
+    const adj = adjInput.value;
+    const adjSpan = document.getElementById('adj');
+    adjSpan.textContent = adj;
+}
